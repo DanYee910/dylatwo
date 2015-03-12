38d5e7a82422e40f0dceb34957066747f0cc3d2f
Daniel Yee <yeedan@gmail.com>
2015-03-11 21:59:03 -0700

    a few more seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,34 +13,45 @@ locations = [
   {name: 'Gas Station', district:'Downtown',fastmin:0, fastmax:1, medmin:1, medmax:2, slowmin:2, slowmax:4, flavortext:'gas is long gone, might be a screwdriver somewhere',imgtag:'none'},
   {name: 'Medical Office', district:'Suburbs',fastmin:0, fastmax:1, medmin:1, medmax:2, slowmin:2, slowmax:4, flavortext:'seems quiet, some bandages would be nice.',imgtag:'none'},
   {name: 'Candle Shop',district:'Suburbs',fastmin:0, fastmax:1, medmin:1, medmax:2, slowmin:2, slowmax:3, flavortext:'Quiet, but is there anything in here?',imgtag:'none'},
+  {name: 'Small House',district:'Suburbs',fastmin:0, fastmax:1, medmin:0, medmax:2, slowmin:1, slowmax:3, flavortext:'Seems safe to look inside.',imgtag:'none'},
+  {name: 'Fire Station',district:'Suburbs',fastmin:0, fastmax:1, medmin:1, medmax:2, slowmin:2, slowmax:3, flavortext:'Might find some emergency supplies.',imgtag:'none'},
   {name:'Seafood Restaurant',district:'Wharf',fastmin:1, fastmax:2, medmin:2, medmax:4, slowmin:3, slowmax:6, flavortext:'big place, lots of zombies...',imgtag:'none'},
   {name:'Bakery',district:'Suburbs',fastmin:0, fastmax:1, medmin:1, medmax:3, slowmin:2, slowmax:4, flavortext:'the smell of baked bread used to linger here',imgtag:'none'},
   {name:'Coffee Shop',district:'Downtown',fastmin:0, fastmax:1, medmin:1, medmax:3, slowmin:2, slowmax:4, flavortext:'Could be some coffee beans somewhere',imgtag:'none'},
   {name:'Police Station',district:'Downtown',fastmin:0, fastmax:1, medmin:2, medmax:3, slowmin:3, slowmax:5, flavortext:'This is not a refuge...',imgtag:'none'},
   {name:'Fine Art Museum',district:'Downtown',fastmin:0, fastmax:1, medmin:1, medmax:2, slowmin:2, slowmax:4, flavortext:'Blood stains the walls, but you see nothing.',imgtag:'none'},
   {name:'Warehouse',district:'Wharf',fastmin:0, fastmax:1, medmin:2, medmax:3, slowmin:2, slowmax:5, flavortext:'Some shipping containers still lying around.',imgtag:'none'},
+  {name:'Bank',district:'Wharf',fastmin:0, fastmax:1, medmin:0, medmax:2, slowmin:0, slowmax:3, flavortext:'Locked up tight, but theres an open window.'},
   {name:'Yacht',district:'Wharf',fastmin:0, fastmax:1, medmin:1, medmax:2, slowmin:2, slowmax:3, flavortext:'Where is the owner?',imgtag:'none'}
 ]
 # item definitions
 items = [
   {name:'Hammer',spawnarea:'All',effect:'Tools: +1',flavortext:'About time one of these showed up',imgtag:'none'},
+  {name:'Soap and Detergent',spawnarea:'All',effect:'Morale: +1',flavortext:'Helps get rid of the smell.',imgtag:'none'},
   {name:'MREs',spawnarea:'All',effect:'Food: +1',flavortext:'Unopened emergency rations.',imgtag:'none'},
   {name:'Broken Computer',spawnarea:'All',effect:'Rare Material: +1',flavortext:'Unfixable, but could be useful parts',imgtag:'none'},
   {name:'Screwdriver Set',spawnarea:'All',effect:'Tools: +1',flavortext:'A well worn set of tools but perfectly functional',imgtag:'none'},
   {name:'Crowbar',spawnarea:'Suburbs',effect:'Attack: +1',flavortext:'Not as much reach as you would like.',imgtag:'none'},
   {name:'Board Games',spawnarea:'Downtown',effect:'Morale: +1',flavortext:"The toy store won't need it anymore",imgtag:'none'},
   {name:'Canned Seafood',spawnarea:'Wharf',effect:'Food: +1',flavortext:"Not the freshest but edible",imgtag:'none'},
+  {name:'Binoculars',spawnarea:'Wharf',effect:'Thorough: +1',flavortext:"Crisp and clear vision",imgtag:'none'},
   {name:'Matress',spawnarea:'Suburbs',effect:'Shelter: +1',flavortext:"A used matress, what could possibly go wrong?",imgtag:'none'},
   {name:'Camera',spawnarea:'Wharf',effect:'Common Materials: +1',flavortext:"Worthless junk...",imgtag:'none'},
   {name:'Herbs and Spices',spawnarea:'Wharf',effect:'Morale: +1',flavortext:"Makes all this crap food more palatable",imgtag:'none'},
   {name:'Food Stash',spawnarea:'All',effect:'Food: +3',flavortext:"Whoever was here left in a hurry.",imgtag:'none'},
+  {name:'Emergency Kit',spawnarea:'All',effect:'Morale: +2',flavortext:"antiseptics, bandages, flares...",imgtag:'none'},
+  {name:'Propane Tank',spawnarea:'Suburbs',effect:'Food: +1',flavortext:"There is a little left.",imgtag:'none'},
   {name:'Crossbow',spawnarea:'Suburbs',effect:'Attack: +1',flavortext:"Includes bolts, convenient!",imgtag:'none'},
+  {name:'Pipe',spawnarea:'All',effect:'Attack: +1',flavortext:"Feel from a sink, serves a new purpose.",imgtag:'none'},
   {name:'Roll of Duct Tape',spawnarea:'Downtown',effect:'Tools: +1',flavortext:"Tape for everything",imgtag:'none'}
 ]
 
 # recipes definitions
 recipes = [
   {name:'Bicycle', starting:'false',tools:2, materials:'Common Material: -2, Uncommon Material: -1', effect: 'Actions: +1', flavortext:'Quiet and efficient transportation, no gas required.'},
+  {name:'Improvised Mortar', starting:'false',tools:3, materials:'Uncommon Material: -3, Rare Material: -2', effect: 'Attack: +2', flavortext:'Loud and destructive, makes a great diversion.'},
+  {name:'Flashlight', starting:'false',tools:1, materials:'Common Material: -1', effect: 'Thorough: +1, Reckless: +1', flavortext:'Helps you see! And be seen...'},
+  {name:'Metal Detector', starting:'false',tools:3, materials:'Uncommon Material: -1, Rare Material: -1', effect: 'Thorough: +1', flavortext:'Bottlecaps, nails, what else...'},
   {name:'Electric Generator', starting:'false',tools:4, materials:'Uncommon Material: -3, Rare Material: -3', effect: 'Shelter: +2, Tools: +1', flavortext:'In good condition, can never get enough electricity.'},
   {name:'Portable stove', starting:'false',tools:2, materials:'Common Material: -1, Uncommon Material: -1', effect: 'Food: +2', flavortext:'Takes a while to warm up, but works.'},
   {name:'Space heater', starting:'false',tools:2, materials:'Common Material: -1, Uncommon Material: -1', effect: 'Shelter: +1', flavortext:'Great for small spaces'},
